cb0a5708fdbd5b7e563a975f7c161cb05d861df0
Dmitry <a89652251224@gmail.com>
2023-12-25 02:07:57 +0300

    Airflow

--- dag.py
+++ dag.py
@@ -1,20 +1,15 @@
-import sys
-
-sys.path.append("/opt/hadoop/airflow/dags/alex_crypto/")
 import datetime as dt
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-import compute.current
-import compute.days
+from main import main
 
-default_args = {"owner": "alex", "start_date": dt.datetime(2023, 12, 14)}
+default_args = {"owner": "alex", "start_date": dt.datetime(2023, 12, 24)}
 
 with DAG(
-    "alex_crypt_current_price",
+    "alex_db_work",
     default_args=default_args,
-    dagrun_timeout=dt.timedelta(minutes=60),
-    schedule_interval="*/5 * * * *",
+    schedule_interval="@dayly",
 ) as dag:
-    air_current = PythonOperator(task_id="current_best_in_telegram", python_callable=compute.current.main)
+    air_db = PythonOperator(task_id="analize_datas", python_callable=main())
