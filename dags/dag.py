b3be061279bf207a2be77f92a28093a8759cc7ba
Dmitry <a89652251224@gmail.com>
2023-12-25 04:12:30 +0300

    airflow

--- dags/dag.py
+++ dags/dag.py
@@ -1,9 +1,12 @@
+import sys
+sys.path.append("/opt/airflow/project/")
+
 import datetime as dt
+from main import main
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-from main import main
 
 default_args = {"owner": "alex", "start_date": dt.datetime(2023, 12, 24)}
 
